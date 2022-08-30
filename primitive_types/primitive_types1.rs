b62d2406e43137ee71d75c3d81d4450edc9c1646
Pan <pancanugrahawicaksana@gmail.com>
2022-08-30 12:59:20 +0700

    Done

--- primitive_types/primitive_types1.rs
+++ primitive_types/primitive_types1.rs
@@ -1,11 +1,8 @@
 // primitive_types1.rs
 // Fill in the rest of the line that has code missing!
 // No hints, there's no tricks, just get used to typing these :)
-
-
 fn main() {
     // Booleans (`bool`)
-
     let is_morning = true;
     if is_morning {
         println!("Good morning!");
