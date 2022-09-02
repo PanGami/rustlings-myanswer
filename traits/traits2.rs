1b52979a2d2b9d6230865e83b704971d7810a739
Pan <pancanugrahawicaksana@gmail.com>
2022-09-02 22:14:21 +0700

    Done

--- traits/traits2.rs
+++ traits/traits2.rs
@@ -10,15 +10,18 @@
 // No boiler plate code this time,
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
-
+impl AppendBar for Vec<String> {
+    fn append_bar(self) -> Self {
+        let mut temp = self.clone();
+        temp.push("Bar".to_string());
+        temp
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
